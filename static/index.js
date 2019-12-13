d5b66703cf6c85ced485ec6981c0d98b117cb574
Odenhobler <56400274+Odenhobler@users.noreply.github.com>
2019-12-13 13:28:23 +0100

    Tabula Rasa

--- static/index.js
+++ static/index.js
@@ -1,4 +1,5 @@
 //      VARIABLEN
+
 let tournamentState; //Magic number hier soll einfach ungleich 0/1/2/3/4, aber auch ein Integer sein, ist also wurst
 let tournamentMode;
 let numberOfSpielfeld;
@@ -7,28 +8,62 @@ let listOfChars;
 
 //      BROWSERSEITE INITIALISIEREN
 
-function switchDivs() {
-    if (tournamentState == 0) {
-        //document.getElementById("sectionlobby").style.display = "none";
-        //document.getElementById("btnaddp").style.display = "none";
-        //document.getElementById("sectionmatches").style.display = "none";
-        //document.getElementById("sectiontree").style.display = "none";
-        //document.getElementById("sectionreset").style.display = "none";
+function godFunction(newState, newMode, newSpielfeld) {    //Bei state=4 nur Abfrage, data ist dann state, modus, spielfeld
+    jQuery.getJSON("/tournamentstate", {tournamentState: newState, tournamentMode: newMode, numberOfSpielfeld: newSpielfeld},
+        function(data) {
+            tournamentState = data.tournamentState;
+            tournamentMode = data.tournamentMode;
+            numberOfSpielfeld = data.numberOfSpielfeld;
+            listOfPlayers = data.listOfPlayers;
+            listOfChars = data.listOfChars;
+            playerListToLobby();
+        });
+}
+
+document.addEventListener('DOMContentLoaded', godFunction(4, 0, 0), false);
+
+//      HEADER
+
+document.getElementById("btnreset").addEventListener('click',resetLobby,false);
+document.getElementById("btnrefresh").addEventListener('click',godFunction(4,0,0),false);
+
+function resetLobby() {
+    //reset lobby
+}
+
+//Twitch frame
+let options = {
+    width: 220,
+    height: 180,
+    channel: "odenhobler"
+  };
+  let player = new Twitch.Player("twitchframe", options);
+  player.setVolume(0.5);
+
+
+//      LOBBY
+
+function addPlayer(){
+    godFunction (4, 0, 0);
+    let newPlayer;
+    let newChars = [];
+    newPlayer =  prompt("Name des Spielers?");
+    for (let i = 0; i < numberOfSpielfeld; i++){
+        char = parseInt(prompt("Charakter " + (i+1) + "?"), 10);
+        newChars.push(char);
     }
-    if (tournamentState == 1) {
-        //document.getElementById("sectionlobby").style.display = "block";
-        document.getElementById("btnaddp").style.display = "block";
-        //document.getElementById("sectionmatches").style.display = "none";
-        //document.getElementById("sectiontree").style.display = "none";
-        //document.getElementById("sectionreset").style.display = "none";
-    }
-    if (tournamentState == 2) {
-        //document.getElementById("sectionlobby").style.display = "none";
-        document.getElementById("btnaddp").style.display = "none";
-        //document.getElementById("sectionmatches").style.display = "block";
-        //document.getElementById("sectiontree").style.display = "none";
-        //document.getElementById("sectionreset").style.display = "none";
-    }
+    $.ajax({
+      type: "POST",
+      contentType: "application/json;charset=utf-8",
+      url: "/listofplayers",
+      traditional: "true",
+      data: JSON.stringify({name: newPlayer, chars: newChars}),
+      dataType: "json",
+      success: function(data) {
+        listOfPlayers = data.listOfPlayers;
+        listOfChars = data.listOfChars;
+        playerListToLobby(); }
+      });
 }
 
 function makeTableHTML(parray, carray) {
@@ -127,171 +162,17 @@ function playerListToLobby(){
     document.getElementById("rt").innerHTML = makeTableHTML(listOfPlayers, listOfChars);
 }
 
-function godFunction(newState, newMode, newSpielfeld) {    //Bei state=4 nur Abfrage, data ist dann state, modus, spielfeld
-    jQuery.getJSON("/tournamentstate", {tournamentState: newState, tournamentMode: newMode, numberOfSpielfeld: newSpielfeld},
-        function(data) {
-            tournamentState = data.tournamentState;
-            tournamentMode = data.tournamentMode;
-            numberOfSpielfeld = data.numberOfSpielfeld;
-            listOfPlayers = data.listOfPlayers;
-            listOfChars = data.listOfChars;
-            playerListToLobby();
-            switchDivs();
-            if (tournamentState == 2) {
-                refreshListOfMatches();
-            }
-            //setTimeout(godFunction, 4999);            //uncommented as it fucked up the whole thing
-        });
-}
-
-document.addEventListener('DOMContentLoaded', godFunction(4, 0, 0), false);
-//document.addEventListener('DOMContentLoaded', switchDivs(), false);            //commented away as it triggered before callback was returned
-
-
-//      HEADER
-
-
-function showDivLobby() {
-    document.getElementById("sectionlobby").style.display = "block";
-    document.getElementById("sectionmatches").style.display = "none";
-    document.getElementById("sectiontree").style.display = "none";
-    document.getElementById("sectionreset").style.display = "none";
-    godFunction(4, 0, 0);
-}
-
-function showDivMatches() {
-    document.getElementById("sectionlobby").style.display = "none";
-    document.getElementById("sectionmatches").style.display = "block";
-    document.getElementById("sectiontree").style.display = "none";
-    document.getElementById("sectionreset").style.display = "none";
-    godFunction(4, 0, 0);
-}
-
-function showDivTree() {
-    document.getElementById("sectionlobby").style.display = "none";
-    document.getElementById("sectionmatches").style.display = "none";
-    document.getElementById("sectiontree").style.display = "block";
-    document.getElementById("sectionreset").style.display = "none";
-    godFunction(4, 0, 0);
-}
-
-function showDivReset() {
-    if (prompt("Passwort") == "d") {
-        document.getElementById("sectionlobby").style.display = "none";
-        document.getElementById("sectionmatches").style.display = "none";
-        document.getElementById("sectiontree").style.display = "none";
-        document.getElementById("sectionreset").style.display = "block";
-        godFunction(4, 0, 0);
-    } else {
-        alert("Passwort falsch");
-    }
-
-}
-
-document.getElementById("btnlobby").addEventListener('click',showDivLobby,false);
-document.getElementById("btnmatches").addEventListener('click',showDivMatches,false);
-document.getElementById("btntree").addEventListener('click',showDivTree,false);
-document.getElementById("btnreset").addEventListener('click',showDivReset,false);
-
-//Twitch frame
-let options = {
-    width: 220,
-    height: 180,
-    channel: "odenhobler"
-  };
-  let player = new Twitch.Player("twitchframe", options);
-  player.setVolume(0.5);
-
-
-//      LOBBY
-
-function addPlayer(){
-    godFunction (4, 0, 0);
-    let newPlayer;
-    let newChars = [];
-    newPlayer =  prompt("Name des Spielers?");
-    for (let i = 0; i < numberOfSpielfeld; i++){
-        char = parseInt(prompt("Charakter " + (i+1) + "?"), 10);
-        newChars.push(char);
-    }
-    $.ajax({
-      type: "POST",
-      contentType: "application/json;charset=utf-8",
-      url: "/listofplayers",
-      traditional: "true",
-      data: JSON.stringify({name: newPlayer, chars: newChars}),
-      dataType: "json",
-      success: function(data) {
-        listOfPlayers = data.listOfPlayers;
-        listOfChars = data.listOfChars;
-        playerListToLobby(); }
-      });
-}
-
-
-//Button Neuer Spieler
+//button new player 
 document.getElementById("btnaddp").addEventListener('click',addPlayer,false);
-
-//      MATCHES
-
-//Refresh function (gets included in god function)
-function refreshListOfMatches() {
-    $.ajax({
-        url: "/listofmatches",
-        success: function(data) {
-          listOfMatches = data.listOfMatches
-          printListOfMatches(); 
-        }
-        });
-    };
-
-//write matches to match section
-function printListOfMatches() {
-    document.getElementById("livelist").innerHTML = listOfMatches;
-}
 
 //      RESET
 
+//just one button, get empty player list
 
-function setToZero() {
-    godFunction(0, 0, 0);
-    switchDivs();
-}
-
-function setToOne() {
-    modus = parseInt(prompt("Welcher Modus? 0=Single, 1=Double, 2=Triple, 3=Liga, 4=Liga+Playoffs"), 10);
-    spielfelds = parseInt(prompt("Wieviele Spielfelder?"), 10);
-    godFunction (1, modus, spielfelds);
-    switchDivs();
-}
-
-function setToTwo() {
-    godFunction(2, 0, 0);
-    switchDivs();
-}
-
+//needed? reset may be fine
 function kickPlayer() {
     player = prompt("Wen?");
     jQuery.getJSON("/kickplayer", {name:player},
             
     )
 }
-
-//Testbutton
-function testAlert() {
-    jQuery.getJSON("/testbutton", {}, 
-    function (data){
-        testAlert = data.testAlert;
-        alert(testAlert);
-        alert(tournamentState);
-    });
-}
-
-document.getElementById("btntest").addEventListener('click',testAlert,false);
-
-//Set Youtube-Link muss noch eingefügt werden
-
-document.getElementById("buttonsetto0").addEventListener('click',setToZero,false);
-document.getElementById("buttonsetto1").addEventListener('click',setToOne,false);
-document.getElementById("buttonsetto2").addEventListener('click',setToTwo,false);
-document.getElementById("buttonkickplayer").addEventListener('click',kickPlayer,false);
