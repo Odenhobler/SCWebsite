947081b625d8ab492b98a216b99a852a385f1fd6
Odenhobler <56400274+Odenhobler@users.noreply.github.com>
2019-12-20 00:08:18 +0100

    retret

--- app.py
+++ app.py
@@ -114,7 +114,6 @@ def add_player():
         return jsonify(alert = "Falsche Anzahl an Charakteren. Eingegeben " + str(len(inp['chars'])) + ", erwartet " + str(cpp) )
     # else: return jsonify(alert = "Need POST method"), 400
     if inp['name'] == 'totalreset':
-        print('hier müsste nun eigentlich ein reset stattfinden')
         reset_lobby()
     print ([listOfPlayers.names,listOfPlayers.chars,listOfPlayers.rseed,listOfPlayers.rfield])
     return jsonify(
