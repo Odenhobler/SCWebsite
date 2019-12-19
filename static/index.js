635608fd6f58cd7f2d4d9011161fc92dc83e74cb
Odenhobler <56400274+Odenhobler@users.noreply.github.com>
2019-12-19 18:19:45 +0100

    Definitiv Funktionsfähig

--- static/index.js
+++ static/index.js
@@ -40,7 +40,9 @@ let options = {
 
 //      LOBBY
 
-function addPlayer(){
+//OLD INT INPUT FORM, (HOPEFULLY) OBSOLETE
+
+/*function addPlayer(){
     godFunction (4, 0, 0);
     let newPlayer;
     let newChars = [];
@@ -63,7 +65,190 @@ function addPlayer(){
         randomField = data.randomField;
         playerListToLobby(); }
       });
-}
+}*/
+
+//new input form
+$('#btnaddp').click(function(){
+    document.getElementById("inputfield").style.display = "block"; 
+});
+
+$('#submitname').click(function(){
+    let newPlayer = ($("#inputname").val());
+    let charone;
+    let chartwo;
+    let newChars = [];
+
+    //transformation dragondrop to integer for Char 1
+    if (($("#charone").val()) == "2B") {
+        charone = 0;
+    }
+    if (($("#charone").val()) == "Amy") {
+        charone = 1;
+    }
+    if (($("#charone").val()) == "Astaroth") {
+        charone = 2;
+    }
+    if (($("#charone").val()) == "Azwell") {
+        charone = 3;
+    }
+    if (($("#charone").val()) == "Cassandra") {
+        charone = 4;
+    }
+    if (($("#charone").val()) == "Cervantes") {
+        charone = 5;
+    }
+    if (($("#charone").val()) == "Geralt") {
+        charone = 6;
+    }
+    if (($("#charone").val()) == "Groh") {
+        charone = 7;
+    }
+    if (($("#charone").val()) == "Hilde") {
+        charone = 8;
+    }
+    if (($("#charone").val()) == "Ivy") {
+        charone = 9;
+    }
+    if (($("#charone").val()) == "Kilik") {
+        charone = 10;
+    }
+    if (($("#charone").val()) == "Maxi") {
+        charone = 11;
+    }
+    if (($("#charone").val()) == "Seong-Mina") {
+        charone = 12;
+    }
+    if (($("#charone").val()) == "Mitsurugi") {
+        charone = 13;
+    }
+    if (($("#charone").val()) == "Nightmare") {
+        charone = 14;
+    }
+    if (($("#charone").val()) == "Raphael") {
+        charone = 15;
+    }
+    if (($("#charone").val()) == "Siggi") {
+        charone = 16;
+    }
+    if (($("#charone").val()) == "Sophitia") {
+        charone = 17;
+    }
+    if (($("#charone").val()) == "Taki") {
+        charone = 18;
+    }
+    if (($("#charone").val()) == "Talim") {
+        charone = 19;
+    }
+    if (($("#charone").val()) == "Tira") {
+        charone = 20;
+    }
+    if (($("#charone").val()) == "Voldo") {
+        charone = 21;
+    }
+    if (($("#charone").val()) == "Xianghua") {
+        charone = 22;
+    }
+    if (($("#charone").val()) == "Yoshimitsu") {
+        charone = 23;
+    }
+    if (($("#charone").val()) == "Zasalamel") {
+        charone = 24;
+    }
+    //same for Char 2
+    if (($("#chartwo").val()) == "2B") {
+        chartwo = 0;
+    }
+    if (($("#chartwo").val()) == "Amy") {
+        chartwo = 1;
+    }
+    if (($("#chartwo").val()) == "Astaroth") {
+        chartwo = 2;
+    }
+    if (($("#chartwo").val()) == "Azwell") {
+        chartwo = 3;
+    }
+    if (($("#chartwo").val()) == "Cassandra") {
+        chartwo = 4;
+    }
+    if (($("#chartwo").val()) == "Cervantes") {
+        chartwo = 5;
+    }
+    if (($("#chartwo").val()) == "Geralt") {
+        chartwo = 6;
+    }
+    if (($("#chartwo").val()) == "Groh") {
+        chartwo = 7;
+    }
+    if (($("#chartwo").val()) == "Hilde") {
+        chartwo = 8;
+    }
+    if (($("#chartwo").val()) == "Ivy") {
+        chartwo = 9;
+    }
+    if (($("#chartwo").val()) == "Kilik") {
+        chartwo = 10;
+    }
+    if (($("#chartwo").val()) == "Maxi") {
+        chartwo = 11;
+    }
+    if (($("#chartwo").val()) == "Seong-Mina") {
+        chartwo = 12;
+    }
+    if (($("#chartwo").val()) == "Mitsurugi") {
+        chartwo = 13;
+    }
+    if (($("#chartwo").val()) == "Nightmare") {
+        chartwo = 14;
+    }
+    if (($("#chartwo").val()) == "Raphael") {
+        chartwo = 15;
+    }
+    if (($("#chartwo").val()) == "Siggi") {
+        chartwo = 16;
+    }
+    if (($("#chartwo").val()) == "Sophitia") {
+        chartwo = 17;
+    }
+    if (($("#chartwo").val()) == "Taki") {
+        chartwo = 18;
+    }
+    if (($("#chartwo").val()) == "Talim") {
+        chartwo = 19;
+    }
+    if (($("#chartwo").val()) == "Tira") {
+        chartwo = 20;
+    }
+    if (($("#chartwo").val()) == "Voldo") {
+        chartwo = 21;
+    }
+    if (($("#chartwo").val()) == "Xianghua") {
+        chartwo = 22;
+    }
+    if (($("#chartwo").val()) == "Yoshimitsu") {
+        chartwo = 23;
+    }
+    if (($("#chartwo").val()) == "Zasalamel") {
+        chartwo = 24;
+    };
+    //document.getElementById("inputfield").style.display = "none";
+    newChars.push(charone);
+    newChars.push(chartwo);
+    $.ajax({
+      type: "POST",
+      contentType: "application/json;charset=utf-8",
+      url: "/listofplayers",
+      traditional: "true",
+      data: JSON.stringify({name: newPlayer, chars: newChars}),
+      dataType: "json",
+      success: function(data) {
+        listOfPlayers = data.listOfPlayers;
+        listOfChars = data.listOfChars;
+        randomSeed = data.randomSeed;
+        randomField = data.randomField;
+        playerListToLobby(); }
+      });
+});
+
 
 function makeTableHTML(parray, carray, rseed, rfield) {
     var result = '<table class="rostertable">';
@@ -164,14 +349,5 @@ function playerListToLobby(){
 
 
 //      BUTTONS 
-document.getElementById("btnaddp").addEventListener('click',addPlayer,false);
+//document.getElementById("btnaddp").addEventListener('click',showInputField(),false);
 //document.getElementById("btnrefresh").addEventListener('click', godFunction(4, 0, 0), false); // doesn't work for whatever reason
-
-
-//needed? reset may be fine
-function kickPlayer() {
-    player = prompt("Wen?");
-    jQuery.getJSON("/kickplayer", {name:player},
-            
-    )
-}
