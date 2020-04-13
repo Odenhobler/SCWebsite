5a7a95450431442f86f28e2f2f13ea3ba84bc910
ArneB42 <57411347+ArneB42@users.noreply.github.com>
2020-04-13 23:09:41 +0200

    conflict

--- app/sc_league/views.py
+++ app/sc_league/views.py
@@ -17,7 +17,7 @@ def blabla(request):
 def ask_for_table(request):
     print("HELLO!!!")
     for p in models.Player.objects.raw('SELECT * FROM league'):
-        print(p.player_name)
+        print(p)
     
     return HttpResponse("but backend tells you to goblablaflask")
 
